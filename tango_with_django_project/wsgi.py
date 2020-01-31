eee26a53367c76886e220402ce87b645bc5937f3
Torrin McQueen <2378609m@student.gla.ac.uk>
2020-01-31 14:07:41 +0000

    Test text removed

--- tango_with_django_project/wsgi.py
+++ tango_with_django_project/wsgi.py
@@ -7,8 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-# Test text
-
 import os
 
 from django.core.wsgi import get_wsgi_application
